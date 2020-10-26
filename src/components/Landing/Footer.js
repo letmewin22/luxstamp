ed1da250653a2b526f1f1d374c9856904483af69
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2020-10-26 18:01:15 +0200

    finish footer

--- src/components/Landing/Footer.js
+++ src/components/Landing/Footer.js
@@ -3,6 +3,28 @@ import {Logo} from '../Logo'
 import {Button} from '../Button/Button'
 
 export const Footer = () => {
+
+  const representations = [
+    {
+      address: 'Івано-Франківськ, пл. Ринок 14 (3 поверх)',
+      googleMaps: '/',
+      phone: '+38 (068) 271-77-78',
+      email: 'luxstampif@gmail.com',
+    },
+    {
+      address: 'Новоград-Волинський, пл. Ринок 14 (3 поверх)',
+      googleMaps: '/',
+      phone: '+38 (068) 271-77-78',
+      email: 'luxstampif@gmail.com',
+    },
+    {
+      address: 'Бахмач, пл. Ринок 14 (3 поверх)',
+      googleMaps: '/',
+      phone: '+38 (068) 271-77-78',
+      email: 'luxstampif@gmail.com',
+    }
+  ]
+
   return (
     <footer className='footer'>
       <div className='container footer__container'>
@@ -10,13 +32,29 @@ export const Footer = () => {
           <Logo />
           <div className='representations'>
             <h6 className='h6 representations-h6'>Представництва</h6>
-            
             <ul className='representations__items'>
-              <li className='representations__li'>
-              <span className='number representations__li-number'>01</span>
-              <p className='representation__li-text'>Обираєте потрібний вид та дизайн печатки.</p>
-              
-              </li>
+              {representations.map((el, i) => {
+                return (
+                  <li key={el.address} className='representations__li'>
+                    <span className='number representations__li-number'>
+                      0{i + 1}
+                    </span>
+                    <p className='representation__li-text'>
+                      <span>
+                        <a href={el.googleMaps}>{el.address}</a>
+                      </span>
+                      <span>
+                        <a href={'tel:' + el.phone.replace(/\D/gm, '')}>
+                          {el.phone}
+                        </a>
+                      </span>
+                      <span>
+                        <a href={'mailto:' + el.email}>{el.email}</a>
+                      </span>
+                    </p>
+                  </li>
+                )
+              })}
             </ul>
           </div>
           <ul className='social'>
--- src/components/Landing/sections/Instruction.js
+++ src/components/Landing/sections/Instruction.js
@@ -1,6 +1,57 @@
 import React from 'react'
 
 export const Instruction = () => {
+
+  const howItems = [
+    {
+      h: 'Для ФОП',
+      text: `Для виготовлення потрібна фотографія/скан-копія витягу з ЕДР або
+      вашого ідентифікаційного коду.`,
+    },
+    {
+      h: 'Для ТОВ',
+      text: `Для держустанов та ТОВ для виготовлення потрібна фотографія або
+      скан-копія витягу з ЕДРПОУ.`,
+    },
+    {
+      h: 'Для адвоката/лікаря',
+      text: `Для виготовлення потрібна фотографія або скан-копія свідоцтва
+      адвоката/диплома лікаря.`,
+    }
+  ]
+
+  const onlineItems = [
+    {
+      text: 'Обираєте потрібний вид та дизайн печатки.',
+    },
+    {
+      text: 'Заповнюєте форму замовлення.',
+    },
+    {
+      text: 'Додаєте до форми фото або скан-копію потрібного документа.',
+    },
+    {
+      text: `Отримуєте виріб кур’єром, Новою Поштою або забираєте у нас в
+      офісі.`,
+    }
+  ]
+
+  const officeItems = [
+    {
+      text: 'Приходите до нашого офісу.',
+    },
+    {
+      text: 'Обираєте потрібний вид та дизайн печатки.',
+    },
+    {
+      text: `Додаєте потрібний документ або його фото/скан-копію, а також
+              документ що підтверджує особу.`,
+    },
+    {
+      text: 'Ви не встигнете допити каву, як ваш виріб буде готовий :)',
+    }
+  ]
+
   return (
     <section className='section instruction'>
       <div className='container section__container instruction__container'>
@@ -10,30 +61,15 @@ export const Instruction = () => {
         </div>
         <div className='instruction__right'>
           <ul className='instruction__items'>
-            <li className='instruction__li'>
-              <div className='section__line' />
-              <h3 className='h3 instruction__h3'>Для ФОП</h3>
-              <p className='instruction__text'>
-                Для виготовлення потрібна фотографія/скан-копія витягу з ЕДР або
-                вашого ідентифікаційного коду.
-              </p>
-            </li>
-            <li className='instruction__li'>
-              <div className='section__line' />
-              <h3 className='h3 instruction__h3'>Для ТОВ</h3>
-              <p className='instruction__text'>
-                Для держустанов та ТОВ для виготовлення потрібна фотографія або
-                скан-копія витягу з ЕДРПОУ.
-              </p>
-            </li>
-            <li className='instruction__li'>
-              <div className='section__line' />
-              <h3 className='h3 instruction__h3'>Для адвоката/лікаря</h3>
-              <p className='instruction__text'>
-                Для виготовлення потрібна фотографія або скан-копія свідоцтва
-                адвоката/диплома лікаря.
-              </p>
-            </li>
+            {howItems.map(item => {
+              return (
+                <li key={item.h} className='instruction__li'>
+                  <div className='section__line' />
+                  <h3 className='h3 instruction__h3'>{item.h}</h3>
+                  <p className='instruction__text'>{item.text}</p>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </div>
@@ -43,23 +79,16 @@ export const Instruction = () => {
           <h4 className='h4 instruction__h4'>Онлайн-замовлення</h4>
           <p className='instruction__text'>4 кроки до готового виробу:</p>
           <ul className='instruction__steps-list'>
-            <li className='instruction__steps-li'>
-              <span className='number instruction__steps-number'>01</span>
-              Обираєте потрібний вид та дизайн печатки.
-            </li>
-            <li className='instruction__steps-li'>
-              <span className='number instruction__steps-number'>02</span>
-              Заповнюєте форму замовлення.
-            </li>
-            <li className='instruction__steps-li'>
-              <span className='number instruction__steps-number'>03</span>
-              Додаєте до форми фото або скан-копію потрібного документа.
-            </li>
-            <li className='instruction__steps-li'>
-              <span className='number instruction__steps-number'>04</span>
-              Отримуєте виріб кур’єром, Новою Поштою або забираєте у нас в
-              офісі.
-            </li>
+            {onlineItems.map((item, i) => {
+              return (
+                <li key={item.text} className='instruction__steps-li'>
+                  <span className='number instruction__steps-number'>
+                    0{i + 1}
+                  </span>
+                  {item.text}
+                </li>
+              )
+            })}
           </ul>
         </div>
         <div className='instruction__right'>
@@ -67,23 +96,16 @@ export const Instruction = () => {
           <h4 className='h4 instruction__h4'>Замовлення в офісі</h4>
           <p className='instruction__text'>4 кроки до готового виробу:</p>
           <ul className='instruction__steps-list'>
-            <li className='instruction__steps-li'>
-              <span className='number instruction__steps-number'>01</span>
-              Приходите до нашого офісу.
-            </li>
-            <li className='instruction__steps-li'>
-              <span className='number instruction__steps-number'>02</span>
-              Обираєте потрібний вид та дизайн печатки.
-            </li>
-            <li className='instruction__steps-li'>
-              <span className='number instruction__steps-number'>03</span>
-              Додаєте потрібний документ або його фото/скан-копію, а також
-              документ що підтверджує особу.
-            </li>
-            <li className='instruction__steps-li'>
-              <span className='number instruction__steps-number'>04</span>
-              Ви не встигнете допити каву, як ваш виріб буде готовий&nbsp;:)
-            </li>
+            {officeItems.map((item, i) => {
+              return (
+                <li key={item.text} className='instruction__steps-li'>
+                  <span className='number instruction__steps-number'>
+                    0{i + 1}
+                  </span>
+                  {item.text}
+                </li>
+              )
+            })}
           </ul>
         </div>
       </div>
